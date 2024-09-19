9fab29f48a772b77d6ef39943104247dcbec5103
Petar Pofuk <ppofuk@gmail.com>
2024-09-19 13:52:18 +0200

    fix: wrong sku value

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin    = "azure"
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard"
     outbound_type     = "loadBalancer"
   }
 }
