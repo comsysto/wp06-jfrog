1d374e04e01c918adb4459a104acbb7e0d2b0510
Petar Pofuk <ppofuk@gmail.com>
2024-09-19 10:20:24 +0200

    add: aks deployment

---
+++ terraform/modules/aks/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "${var.name}-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "Standard"
+    outbound_type     = "loadBalancer"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+}
