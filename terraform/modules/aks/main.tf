0bd292f3ec4a4417557d1b84ea7b3647b06fc5fa
p.pofuk <p.pofuk@reply.de>
2024-09-20 18:01:28 +0200

    fix: move to standard tier due to capacity issue

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -21,6 +21,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = var.dns_prefix
+  sku_tier            = "Standard"
 
   default_node_pool {
     name       = "default"
